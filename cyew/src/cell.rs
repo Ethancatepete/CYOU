8f0b1e9d6ca0d4b0ce3e65a8c6f179dad9941e8a
Ethan <cheunt3@kgv.hk>
2023-07-20 21:22:52 +0800

    Added full annotations to each detail

--- cyew/src/cell.rs
+++ cyew/src/cell.rs
@@ -1,5 +1,7 @@
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum State {
+
+//Either two states
+#[derive(Clone, Copy, PartialEq, Eq)] //Duplicate the same with partial equity(a!=b) and !(a==b), and equity (x==y)
+pub enum State { //data in different states; it is a interface
     Alive,
     Dead,
 }
@@ -10,22 +12,27 @@ pub struct Cellule {
 }
 
 impl Cellule {
+    //by default some are dead
     pub fn new_dead() -> Self {
         Self { state: State::Dead }
     }
 
+    //some are alive
     pub fn set_alive(&mut self) {
         self.state = State::Alive;
     }
 
+    //set those alive to dead
     pub fn set_dead(&mut self) {
         self.state = State::Dead;
     }
 
+    //checks those alive cells are still alive
     pub fn is_alive(self) -> bool {
         self.state == State::Alive
     }
 
+    //function that keeps game running
     pub fn toggle(&mut self) {
         if self.is_alive() {
             self.set_dead()
@@ -34,18 +41,25 @@ impl Cellule {
         }
     }
 
+//this is the area where the rules of the game is created -- need to modify below sections
+
+    //counts alive neighbors around itself, contained inside an array which has a dynamic siae
+    //stored on the heap
     pub fn count_alive_neighbors(neighbors: &[Self]) -> usize {
         neighbors.iter().filter(|n| n.is_alive()).count()
     }
-
+    
+    //if alive neighbors less than 2 - it is "Alone" (True)
     pub fn alone(neighbors: &[Self]) -> bool {
         Self::count_alive_neighbors(neighbors) < 2
     }
 
+    //if alive neighbors greater than 3 - it is "Over-populated" (True)
     pub fn overpopulated(neighbors: &[Self]) -> bool {
         Self::count_alive_neighbors(neighbors) > 3
     }
 
+    //if alive neibors equal to 3 - it "can_be_revived" (True)
     pub fn can_be_revived(neighbors: &[Self]) -> bool {
         Self::count_alive_neighbors(neighbors) == 3
     }
