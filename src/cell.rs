02832321982f1aaff39b5dc6f7593fb1598ced70
wylited <44154103+wylited@users.noreply.github.com>
2023-09-04 22:25:58 +0800

    file directory reorganization

---
+++ src/cell.rs
@@ -0,0 +1,29 @@
+//Either two states
+pub type State = char;
+
+#[derive(Clone, Copy)]
+pub struct Cellule {
+    pub state: State,
+}
+
+impl Cellule {
+    pub fn new(state: State) -> Self {
+        Self { state }
+    }
+
+    pub fn set_state(&mut self, state: State) {
+        self.state = state;
+    }
+
+    pub fn set_blank(&mut self) {
+        self.state = 'A';
+    }
+
+    //function that keeps game running
+    pub fn toggle_cell(&mut self, state: State) {
+        match self.state {
+            'A' => self.state = state,
+            _ => self.state = 'A',
+        }
+    }
+}
